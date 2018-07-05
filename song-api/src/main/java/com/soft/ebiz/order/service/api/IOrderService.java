5249e60afbc09cfb83c1fa0be6ca08f24d598bdb
raolesong <raolesong@LH900362.dds.com>
2018-07-05 15:20:57 +0800

    a

---
+++ song-api/src/main/java/com/soft/ebiz/order/service/api/IOrderService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C), 2013-2018, 上汽集团
+ * FileName: IOrderService.java
+ * Author:   raolesong
+ * Date:     2018年7月5日 下午2:35:15
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.soft.ebiz.order.service.api;
+
+/**
+ * 〈功能详细描述〉
+ * @author raolesong
+ * 2018年7月5日 下午2:35:15
+ */
+public interface IOrderService {
+	
+	int addOrder();
+
+}
---
+++ song-service/src/main/java/com/soft/ebiz/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C), 2013-2018, 上汽集团
+ * FileName: OrderServiceImpl.java
+ * Author:   raolesong
+ * Date:     2018年7月5日 下午2:42:03
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.soft.ebiz.order.service.impl;
+import com.soft.ebiz.order.service.api.IOrderService;
+
+/**
+ * 〈功能详细描述〉
+ * @author raolesong
+ * 2018年7月5日 下午2:42:03
+ */
+public class OrderServiceImpl implements IOrderService {
+
+	public int addOrder() {
+		return 0;
+	}
+
+}
